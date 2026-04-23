c5fed3dd147348c7e86f080e13033ffbc672d353
lucas <lucasteichert.5in@gmail.com>
2026-04-23 15:51:03 +0800

    calculater Stage 1

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,10 +1,31 @@
+//Make Variables
 
-let counterbutton = document.getElementById("counter-button");
 let counter = document.getElementById("counter");
 let number = 0;
+let counterbutton = document.getElementById("add-1-counter-button");
+let minusbutton = document.getElementById("minus-1-counter-button");
+let clearbutton = document.getElementById("clear-button")
+
+// Define functions
 function add1() {
     number = number + 1;
     counter.textContent = number;
 }
 
-counterbutton.addEventListener("click",add1)
+function minus1() {
+    number = number - 1;
+    counter.textContent = number;
+}
+function clear1() {
+    number = 0;
+    counter.textContent = number;
+}
+
+
+//Define Button Event Listeners
+counterbutton.addEventListener("click", add1);
+minusbutton.addEventListener("click", minus1);
+clearbutton.addEventListener("click", clear1)
+
+
+
